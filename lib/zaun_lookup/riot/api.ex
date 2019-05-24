64a662e5136f3342b44cfca797af7f7ffda77b7d
amadorzcv <lucasamadorzcv@gmail.com>
2019-05-24 02:21:31 -0300

    Setup api

--- lib/zaun_lookup/riot/api.ex
+++ lib/zaun_lookup/riot/api.ex
@@ -1,3 +1,27 @@
 defmodule ZaunLookup.Riot.Api do
+  alias ZaunLookup.Riot.Routes
 
+  def request(url) do
+    headers = ["X-Riot-Token": "RGAPI-4da6aa59-c677-493f-ab26-5ebecfdd3241"]
+    HTTPoison.get!(url, headers)
+  end
+
+  def get_summoner_by_name(region, name) do
+    url = Routes.summoner_by_summoner_name(region, name)
+    request(url)
+  end
+
+  def get_summoner_by_puuid(region, puuid) do
+    url = Routes.summoner_by_puuid(region, puuid)
+    request(url)
+  end
+  def get_match_by_id(region, match_id) do
+    url = Routes.match_by_match_id(region, match_id)
+    request(url)
+  end
+
+  def get_matches_by_account_id(region, account_id) do
+    url = Routes.matches_by_account_id(region, account_id)
+    request(url)
+  end
 end
--- lib/zaun_lookup/riot/routes.ex
+++ lib/zaun_lookup/riot/routes.ex
@@ -14,17 +14,17 @@ defmodule ZaunLookup.Riot.Routes do
   end
 
   @match "/lol/match/v4/"
-  @match_by_account_id "#{@match}matchlists/by-account/"
-  @matches_by_match_id "#{@match}matches/"
+  @matches_by_account_id "#{@match}matchlists/by-account/"
+  @match_by_match_id "#{@match}matches/"
 
-  def match_by_account_id(region,account_id) do
+  def matches_by_account_id(region,account_id) do
     endpoint = Endpoints.get_endpoint(region)
-    "#{endpoint}#{@match_by_account_id}#{account_id}"
+    "#{endpoint}#{@matches_by_account_id}#{account_id}"
   end
 
-  def matches_by_match_id(region,match_id) do
+  def match_by_match_id(region,match_id) do
     endpoint = Endpoints.get_endpoint(region)
-    "#{endpoint}#{@matches_by_match_id}#{match_id}"
+    "#{endpoint}#{@match_by_match_id}#{match_id}"
   end
 
 end
