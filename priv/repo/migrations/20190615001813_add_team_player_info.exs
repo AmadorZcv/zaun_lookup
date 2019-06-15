7b55949b09b052afde9ac3ea2395def72e036bca
amadorzcv <lucasamadorzcv@gmail.com>
2019-06-14 21:22:24 -0300

    commit anterior

--- priv/repo/migrations/20190615001813_add_team_player_info.exs
+++ priv/repo/migrations/20190615001813_add_team_player_info.exs
@@ -2,6 +2,12 @@ defmodule ZaunLookup.Repo.Migrations.AddTeamPlayerInfo do
   use Ecto.Migration
 
   def change do
-
+    alter table(:teams_players) do
+      add :champion_id, :integer
+      add :spell_1, :integer
+      add :spell_2, :integer
+      add :lane, :string
+      add :role, :string
+    end
   end
 end
